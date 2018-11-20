a6ab9e39c41e34ca49bcb682a8cb698fdcbcb2fd
Ben Grandin <bengrandin@hotmail.com>
2018-11-20 16:27:31 +0100

    morpion done

--- morpion/morpion.js
+++ morpion/morpion.js
@@ -34,6 +34,7 @@ document.getElementById('morpion').children[2].children[2].innerHTML = 'I';
 var morpion = [[],[],[]];
 var turn = 'X';
 var win = false;
+var a = 0;
 // Ne rien modifier au dessus de ce commentaire
 
 // Fonction qui permet de jouer
@@ -54,13 +55,14 @@ function play(x, y) {
 	}
 }
 
+
 // Fonction qui permet de recommencer
 function restart() {
 	morpion = [[],[],[]];
 	turn = 'X';
 	win = false;
 	// Ne rien modifier au dessus de ce commentaire
-
+a = 0;
 	// Ne rien modifier au dessous de ce commentaire
 	for (var y = 0; y <= 2; y++) {
 		for (var x = 0; x <= 2; x++) {
@@ -70,9 +72,56 @@ function restart() {
 	document.getElementById('infos').children[0].innerHTML = 'Au tour de X';
 }
 
+
 // Fonction de vérification de victoire
 function is_win(x, y) {
 	// Ne rien modifier au dessus de ce commentaire
+	a += 1;
+	console.log("x : " + x + " y :" + y);
+
+
+	if (morpion[0][x] == morpion[1][x] && morpion[1][x]  == morpion[2][x])
+	{
+		win = morpion[y][x];
+		
+	}
+	else if (morpion[y][0] == morpion[y][1] && morpion[y][1] == morpion[y][2])
+	{
+		win = morpion[y][x];
+
+	}
+
+	// only the fisrt diag work
+	else if 
+		(
+			morpion[1][1] != undefined &&
+			(
+				(x == 0 && y == 0) ||
+				(x == 1 && y == 1) ||
+				(x == 2 && y == 2) ||
+				(x == 0 && y == 2) ||
+				(x == 2 && y == 0)
+			)
+		)
+	{
+		if (morpion[0][0] == morpion[1][1] && morpion[1][1] == morpion[2][2])
+		{
+			win = morpion[y][x];
+
+		}
+		else if(morpion[2][0] == morpion[1][1] && morpion[1][1] == morpion[0][2])
+		{
+			win = morpion[y][x];
+
+		}
+	}
+	else if (a == 9)
+	{
+		win = 'null';
+	}
+
+
+
 
 	// Ne rien modifier au dessous de ce commentaire
 	if (win == 'null') {
